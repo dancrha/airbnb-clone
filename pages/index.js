9fd024fbe3ede75cb70c329e746da87be6e2b272
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-03 18:30:38 -0400

    header done

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function Home() {
       <Header />
 
       {/* Banner */}
+      <Banner />
     </div>
   );
 }
