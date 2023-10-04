5b3a21c603318f130ce4f6eb07f14f1f50bfd19f
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-04 18:14:47 -0400

    pulled data from user into search page

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,29 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { format } from "date-fns";
+import { useRouter } from "next/router";
 
 function Search() {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), "MMMM dd, yyyy");
+  const formattedEndDate = format(new Date(endDate), "MMMM dd, yyyy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+  console.log(location);
   return (
     <div>
       <Header />
 
       <main className='flex'></main>
       <section className='flex-grow pt-14 px-6'>
-        <p className='text-xs'>300+ Stays for ... guests</p>
-        <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+        <p className='text-xs'>
+          300+ Stays - {range} - for {noOfGuests} guests
+        </p>
+        <h1 className='text-3xl font-semibold mt-2 mb-6'>
+          Stays in {location}
+        </h1>
 
         <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
           <p className='button hover:scale-105'>Cancellation Flexibility</p>
