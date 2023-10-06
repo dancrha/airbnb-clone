c5b62f0fbd7226fbcf21f0e1cc38130bc007ba20
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-05 21:28:20 -0400

    markers not showing up

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,27 +1,45 @@
-import Map from "react-map-gl";
+import Map, { Marker, Popup } from "react-map-gl";
 import getCenter from "geolib/es/getCenter";
+import { useState } from "react";
 
 function MapComponent({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   const coordinates = searchResults.map((result) => ({
     longitude: result.long,
     latitude: result.lat,
   }));
 
   const center = getCenter(coordinates);
-  console.log(center);
+
   return (
     <Map
       mapStyle='mapbox://styles/dc1708/clndpb9kr07s101ma77w6hnn9'
       mapboxAccessToken={process.env.mapbox_key}
-      mapLib={import("mapbox-gl")}
       initialViewState={{
-        latitude: center.latitude,
         longitude: center.longitude,
+        latitude: center.latitude,
         zoom: 11,
       }}
-      style={{ width: "100%", height: "100%" }}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    />
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className='cursor-pointer text-2xl animate-bounce'
+            >
+              📍test
+            </p>
+          </Marker>
+        </div>
+      ))}
+    </Map>
   );
 }
 
